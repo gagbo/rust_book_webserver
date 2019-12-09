b4eaffd38f93929c6fd081e3eecd48b96cf0a967
Gerry Agbobada <gagbobada+git@gmail.com>
2019-12-10 00:21:15 +0100

    webserv with debug

---
+++ src/bin/main.rs
@@ -0,0 +1,45 @@
+use rust_book_webserv::ThreadPool;
+use std::{
+    fs,
+    io::prelude::*,
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(8) {
+        println!("Received : {:?}", stream);
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+    println!("Unwrapped : {:?} into \n\t{:?}", stream, std::str::from_utf8(&buffer[..]).unwrap());
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(6));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    println!("{}{}", status_line, contents);
+    let response = format!("{}{}", status_line, contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
